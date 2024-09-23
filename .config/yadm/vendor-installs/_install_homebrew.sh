1c0a21d78d58e4a086c32ff132c1baf64c68e6d0
Benjamin Bini <benjamin@bini.io>
2024-09-24 01:03:14 +0200

    Fix brew install

--- .config/yadm/vendor-installs/_install_homebrew.sh
+++ .config/yadm/vendor-installs/_install_homebrew.sh
@@ -2,6 +2,6 @@
 if ! command -v brew &> /dev/null; then 
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh >> /tmp/install-homebrew.sh
   sudo chmod +x /tmp/install-homebrew.sh
-  sudo NONINTERACTIVE=1 /bin/bash -c /tmp/install-homebrew.sh
+  NONINTERACTIVE=1 /bin/bash -c /tmp/install-homebrew.sh
   sudo rm /tmp/install-homebrew.sh
 fi
